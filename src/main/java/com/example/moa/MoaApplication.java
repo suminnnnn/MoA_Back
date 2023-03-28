34e9da90e8587db2cc5a8c97142da5f86c9adfb1
suminnnnn <tlsssm1212@ajou.ac.kr>
2023-03-28 23:48:59 +0900

    init

--- src/main/java/com/example/moa/MoaApplication.java
+++ src/main/java/com/example/moa/MoaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MoaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoaApplication.class, args);
 	}
