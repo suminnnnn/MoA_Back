1dbf6af3198be037e36b342aff3fbd17b76bc157
suminnnnn <tlsssm1212@ajou.ac.kr>
2023-04-02 10:12:23 +0900

    jwt

---
+++ src/main/java/com/example/moa/controller/JwtTokenUtil.java
@@ -0,0 +1,49 @@
+package com.example.moa.controller;
+
+import com.example.moa.domain.User;
+import com.example.moa.service.AuthService;
+import com.example.moa.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenUtil {
+    private String secretKey = "secretKey";
+    private long tokenValidTime = 30 * 60 * 1000L;
+    Date now = new Date();
+
+    //Base64로 인코딩
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String generateToken(User user) {
+        Claims claims = Jwts.claims()
+                .setSubject("access_token")
+                .setIssuedAt(now) //생성일 설정
+                .setExpiration(new Date(now.getTime() + tokenValidTime)); //만료일 설정
+
+        claims.put("key", "value");
+
+        String jwt = Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .signWith(SignatureAlgorithm.HS256,  secretKey.getBytes())  // 사용할 암호화 알고리즘과
+                // signature 에 들어갈 secret값 세팅
+                .compact();
+
+        return jwt;
+    }
+}
