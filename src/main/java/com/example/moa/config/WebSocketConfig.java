017918bc9405d43acbf9a8913e141102e907fa13
Jeong hyunji <azureblue@ajou.ac.kr>
2023-05-25 17:33:40 +0900

    tlqkf

--- src/main/java/com/example/moa/config/WebSocketConfig.java
+++ src/main/java/com/example/moa/config/WebSocketConfig.java
@@ -1,14 +1,14 @@
-package com.example.moa.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-
-@Component
-public class WebSocketConfig {
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
-}
+//package com.example.moa.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+//
+//@Component
+//public class WebSocketConfig {
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
+//}
