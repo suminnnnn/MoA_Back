c7e2227169d544ff2f60fd26b0f3b815b62b5d1b
Jeong hyunji <azureblue@ajou.ac.kr>
2023-05-28 17:18:43 +0900

    why

--- src/main/java/com/example/moa/ChatServerApplication.java
+++ src/main/java/com/example/moa/ChatServerApplication.java
@@ -1,19 +1,19 @@
-//package com.example.moa;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-//
-//@SpringBootApplication
-//public class ChatServerApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(ChatServerApplication.class, args);
-//    }
-//
+package com.example.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@SpringBootApplication
+public class ChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerApplication.class, args);
+    }
+
 //    @Bean
 //    public ServerEndpointExporter serverEndpointExporter() {
 //        return new ServerEndpointExporter();
 //    }
-//}
+}
