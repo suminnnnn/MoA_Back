e3ff28ad7684a500d310c3170582e9650ff78b5f
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2023-06-26 22:27:06 +0900

    Update ChatController.java

--- src/main/java/com/example/moa/controller/ChatController.java
+++ src/main/java/com/example/moa/controller/ChatController.java
@@ -1,30 +1,30 @@
-//package com.example.moa.controller;
-//
-//import com.example.moa.domain.ChatMessage;
-//import com.example.moa.dto.chat.ChatMessageRequestDto;
-//import com.example.moa.dto.chat.ChatMessageResponseDto;
-//import com.example.moa.service.ChatService.ChatService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.messaging.handler.annotation.DestinationVariable;
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.messaging.simp.annotation.SubscribeMapping;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//public class ChatController {
-//
-//    @Autowired
-//    private final SimpMessagingTemplate messagingTemplate;
-//    @Autowired
-//    private final ChatService chatService;
-//
+package com.example.moa.controller;
+
+import com.example.moa.domain.ChatMessage;
+import com.example.moa.dto.chat.ChatMessageRequestDto;
+import com.example.moa.dto.chat.ChatMessageResponseDto;
+import com.example.moa.service.ChatService.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+   @Autowired
+   private final SimpMessagingTemplate messagingTemplate;
+   @Autowired
+   private final ChatService chatService;
+
 //    @MessageMapping("/chat.sendMessage")
 //    public void sendMessage(@RequestBody ChatMessageRequestDto chatMessageDto) {
 //        // 채팅 메시지를 저장
@@ -42,12 +42,12 @@
 //        messagingTemplate.convertAndSend(topic, "New user subscribed: " + sessionId);
 //    }
 //
-//    @GetMapping("/chat/{roomId}/messages")
-//    public ResponseEntity<List<ChatMessageResponseDto>> getChatMessages(@PathVariable String roomId) {
-//        List<ChatMessageResponseDto> chatMessages = chatService.getChatMessagesByRoomId(roomId);
-//        return ResponseEntity.ok(chatMessages);
-//    }
-//
-//}
-//
-//
+   @GetMapping("/chat/{roomId}/messages")
+   public ResponseEntity<List<ChatMessageResponseDto>> getChatMessages(@PathVariable String roomId) {
+       List<ChatMessageResponseDto> chatMessages = chatService.getChatMessagesByRoomId(roomId);
+       return ResponseEntity.ok(chatMessages);
+   }
+
+}
+
+
